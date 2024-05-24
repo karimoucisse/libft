df92b5956a545723f65838833df3eb3b46f8d2a5
karimou cisse <karimou.cisse1@gmail.com>
2024-05-24 22:23:07 +0200

    add ft_colloc.c file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kcisse <kcisse@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 19:45:24 by kcisse            #+#    #+#             */
-/*   Updated: 2024/05/24 21:01:36 by kcisse           ###   ########.fr       */
+/*   Updated: 2024/05/24 22:22:22 by kcisse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ void	*ft_memcpy(void *dst, const void *src, int size);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *p1, const void *p2, int size);
 void	*ft_memmove(void *dst, const void *src, size_t n);
-//void	*ft_calloc(size_t nmemb, size_t size); // NO
+void	*ft_calloc(size_t nmemb, size_t size); // NO
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putchar_fd(char c, int fd);
