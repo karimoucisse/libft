3746e711e3eff0ec593a2adc060edbe211e10fd1
karimou cisse <karimou.cisse1@gmail.com>
2024-05-27 11:40:02 +0200

    fixed Makefile

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ NAME = libft.a
 RM = rm -f
 CC = cc
 CFLAGS = -Wall -Werror -Wextra
-# BONUS_CFILES = ft_lstnew_bonus.c
+BONUS_CFILES = ft_lstnew_bonus.c
 SUP_CFILES = ft_substr.c ft_strjoin.c ft_strtrim.c ft_split.c ft_itoa.c\
 	ft_putendl_fd.c ft_putchar_fd.c ft_putstr_fd.c ft_putnbr_fd.c\
 	ft_strmapi.c ft_striteri.c
@@ -10,24 +10,20 @@ CFILES = ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c ft_isprint.c \
 	ft_strlen.c  ft_tolower.c ft_toupper.c ft_strchr.c ft_strrchr.c \
 	ft_strncmp.c ft_strnstr.c ft_atoi.c ft_strlcpy.c ft_strlcat.c \
 	ft_strdup.c ft_bzero.c ft_memset.c ft_memcpy.c ft_memchr.c ft_memcmp.c \
-	ft_memmove.c ft_calloc.c $(SUP_CFILES)
+	ft_memmove.c ft_calloc.c $(SUP_CFILES) $(BONUS_CFILES)
 OFILES = $(CFILES:.c=.o)
-# BONUS_OFILES = $(BONUS_CFILES:_bonus.c=.o)
 
-# bonus: $(BONUS_OFILES)
-# 	ar rcs bonus $(BONUS_OFILES)
-# %.o:%_bonus.c
-# 	$(CC) $(CFLAGS) -c $< -o $@
-$(NAME): $(OFILES)
-	ar rcs $(NAME) $(OFILES)
+all: $(NAME)
 %.o:%.c
 	$(CC) $(CFLAGS) -c $< -o $@
-all: $(NAME)
+$(NAME): $(OFILES)
+	ar rcs $(NAME) $(OFILES)
+bonus: $(OFILES)
+	ar rcs bonus $(OFILES)
 clean:
 	$(RM) $(OFILES)
-# $(RM) $(BONUS_OFILES)
 fclean: clean
 	$(RM) $(NAME)
-# $(RM) bonus
+	$(RM) bonus
 re: fclean all
 .PHONY: all clean fclean re
--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcisse <kcisse@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/27 11:39:28 by kcisse            #+#    #+#             */
+/*   Updated: 2024/05/27 11:39:29 by kcisse           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-t_list *ft_lstnew(void *content)
+t_list	*ft_lstnew(void *content)
 {
-	t_list *first_list;
+	t_list	*first_list;
 
-	first_list = malloc(sizeof(t_list *));
-	if(first_list == NULL)
+	first_list = malloc(sizeof(t_list));
+	if (first_list == NULL)
 		return (0);
 	first_list->content = content;
 	first_list->next = NULL;
