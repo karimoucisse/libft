6e4d793575e8bde5e7281a4b99e8d14033d41784
Karimou Cisse <kcisse@paul-f3Ar2s4.clusters.42paris.fr>
2024-05-29 19:20:31 +0200

    add change in ft_lstmap file

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: kcisse <kcisse@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 20:07:15 by kcisse            #+#    #+#             */
-/*   Updated: 2024/05/29 18:13:07 by kcisse           ###   ########.fr       */
+/*   Updated: 2024/05/29 19:14:16 by kcisse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,21 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new_lst;
 	t_list	*lst_copy;
 
-	if(!lst)
+	if (!lst)
 		return (0);
-	while(lst)
+	while (lst)
 	{
-		if(ft_lstnew((*f)(lst->content)))
+		if (ft_lstnew((*f)(lst->content)))
+		{
 			lst_copy = ft_lstnew((*f)(lst->content));
+			ft_lstadd_back(&new_lst, lst_copy);
+			// new_lst = new_lst->next;
+		}
 		else
 		{
 			(*del)(lst_copy->content);
 			free(lst_copy);
 		}
-		ft_lstadd_back(&new_lst, lst_copy);
 		lst = lst->next;
 	}
 	return (new_lst);
