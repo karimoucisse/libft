fc1a4769c5df9f4a01209c4430f683bd0a6d4210
Karimou Cisse <kcisse@paul-f3Ar2s4.clusters.42paris.fr>
2024-05-29 18:00:57 +0200

    fixed ft_substr function

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: kcisse <kcisse@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 20:55:24 by kcisse            #+#    #+#             */
-/*   Updated: 2024/05/28 16:45:19 by kcisse           ###   ########.fr       */
+/*   Updated: 2024/05/29 18:00:27 by kcisse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,21 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	i = 0;
 	if (!s)
 		return (0);
-	if ((unsigned int)start > ft_strlen(s))
+	if (start > ft_strlen(s))
 		len = 0;
-	s = s + start;
-	if (len > ft_strlen(s))
-		len = ft_strlen(s);
+	else if (len > ft_strlen(&s[start]))
+		len = ft_strlen(&s[start]);
 	new_str = malloc(sizeof(char) * (len + 1));
 	if (!new_str)
 		return (0);
-	while (*s && i < (len))
+	if (start <= ft_strlen(s))
 	{
-		new_str[i] = *s;
-		i++;
-		s++;
+		while (s[start] && i < (len))
+		{
+			new_str[i] = s[start];
+			i++;
+			start++;
+		}
 	}
 	new_str[i] = '\0';
 	return (new_str);
